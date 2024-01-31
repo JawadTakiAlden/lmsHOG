6cf2b15679b9be4fedb5f830b39d9eb170d19ad1
JawadTakiAlden <101710481+JawadTakiAlden@users.noreply.github.com>
2024-01-31 18:00:34 -0500

    fix server endpont

--- src/api/request.js
+++ src/api/request.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 // import Cookies from "js-cookie";
-const client = axios.create({ baseURL: "https://houseofgeniuses.tech/api/v1"});
+const client = axios.create({ baseURL: "https://api.houseofgeniuses.tech/api/v1"});
 export const request = async ({ ...options }) => {
   client.defaults.headers.common.Authorization = `Bearer ${localStorage.getItem('token_admin_house_of_geniuses')}`;
   return client(options).then((res) => res);
