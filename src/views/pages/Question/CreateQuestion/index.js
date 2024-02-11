1140ac9eb47f611177d025c9022812182d970819
JawadTakiAlden <101710481+JawadTakiAlden@users.noreply.github.com>
2024-02-11 14:52:19 -0500

    try questions

--- src/views/pages/Question/CreateQuestion/index.js
+++ src/views/pages/Question/CreateQuestion/index.js
@@ -1,25 +1,15 @@
-
-import '//unpkg.com/mathlive'
 import { useState, useRef, useEffect } from 'react'
 const CreateQuestion = () => {
   const [value, setValue] = useState('\\sqrt{x}')
-
-  // Customize the mathfield when it is mounted
   const mf = useRef()
   useEffect(() => {
-    // Read more about customizing the mathfield: https://cortexjs.io/mathlive/guides/customizing/
     mf.current.smartFence = true
-
-    // This could be an `onInput` handler, but this is an alternative
     mf.current.addEventListener('input', (evt) => {
-      // When the return key is pressed, play a sound
       if (evt.inputType === 'insertLineBreak') {
         evt.target.executeCommand('plonk')
       }
     })
   }, [])
-
-  // Update the mathfield when the value changes
   useEffect(() => {
     mf.current.value = value
   }, [value])
