b0be87da29cedf4e391cdfa4cc92f15f22ba4bbd
JawadTakiAlden <jawad.taki.alden2002@gmail.com>
2024-08-24 13:13:58 +0300

    fix api

--- src/api/request.js
+++ src/api/request.js
@@ -3,10 +3,10 @@ import axios from "axios";
 import CryptoJS from 'crypto-js';
 import i18next from "i18next";
 
-const baseURl = "https://api.houseofgeniuses.tech/api/v1"
+const baseURL = "https://api.houseofgeniuses.tech/api/v1"
 // const baseURl = "http://127.0.0.1:8000/api/v1"
 
-const client = axios.create({ baseURl });
+const client = axios.create({ baseURL });
 export const request = async ({ ...options }) => {
   // const {token} = JSON.parse(CryptoJS.AES.decrypt(localStorage.getItem('token_admin_house_of_geniuses'), "26_@Jawad0909@_19").toString(CryptoJS.enc.Utf8));
   client.defaults.headers.common.Authorization = `Bearer ${localStorage.getItem('token_admin_house_of_geniuses')}`;
