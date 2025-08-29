21203842c44569f24467c49bed085713adee9829
Jawad2003Taki <jawad2002taki@gmail.com>
2025-08-29 17:26:22 +0300

    switch from cra to vite

---
+++ src/assets/asstetsExporter.jsx
@@ -0,0 +1,6 @@
+import Login from "./images/login.png";
+import Loading from "./images/loading.gif";
+export const images = { Login, Loading };
+import Logo from "./images/logo-image.png";
+
+export const logo = Logo;
